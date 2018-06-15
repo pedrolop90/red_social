ea7eba0b54b27b89e020e35b9b07eb12c0fecceb
pedrolop90 <pedrolop90@hotmail.com>
2018-06-14 20:47:18 -0500

    0.1.4
    version estable para los desarrolladores

--- src/main/java/com/proyecto/arq/repository/RCantidad.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.arq.repository;
-
-import com.proyecto.arq.entity.Cantidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RCantidad extends JpaRepository<Cantidad, Integer> {
-
-}
