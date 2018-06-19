493d7bc9d3edbba419f827b7e96ede2c3cca5da3
pedrolop90 <pedrolop90@hotmail.com>
2018-06-19 11:15:41 -0500

    0.2.4

---
+++ src/main/java/com/proyecto/arq/repository/RPaso_Ingrediente.java
@@ -0,0 +1,6 @@
+package com.proyecto.arq.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RPaso_Ingrediente extends JpaRepository<RPaso_Ingrediente,Integer> {
+}
