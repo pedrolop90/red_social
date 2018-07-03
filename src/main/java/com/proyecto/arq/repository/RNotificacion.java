bdf1f6fe28a66417156263c576b395156a3f74bf
pedrolop90 <pedrolop90@hotmail.com>
2018-07-02 22:04:08 -0500

    0.5.7

--- src/main/java/com/proyecto/arq/repository/RNotificacion.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.arq.repository;
-
-import com.proyecto.arq.entity.Comentario;
-import com.proyecto.arq.entity.Notificacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RNotificacion extends JpaRepository<Notificacion, Integer> {
-
-}
--- src/main/java/com/proyecto/arq/repository/RTipo.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.arq.repository;
-
-import com.proyecto.arq.entity.Tipo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RTipo extends JpaRepository<Tipo, Integer> {
-
-}
